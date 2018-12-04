3b3d8834d2b955d38dabd3b926b95eb33d48ee45
Michal Bielik <m.bielik@insign.ch>
2018-12-04 15:21:43 +0100

    Foo added

--- src/main/java/sk/scarabeon/revolut/api/domain/Account.java
+++ src/main/java/sk/scarabeon/revolut/api/domain/Account.java
@@ -7,6 +7,7 @@ public class Account {
 	private long id;
 	private Currency currency;
 	private double balance;
+	private String foo;
 
 	public Account(long id, Currency currency, double balance) {
 		setId(id);
@@ -36,5 +37,13 @@ public class Account {
 
 	public void setBalance(double balance) {
 		this.balance = balance;
+	}
+
+	public String getFoo() {
+		return foo;
+	}
+
+	public void setFoo(String foo) {
+		this.foo = foo;
 	}
 }
